ba99e2500becddff6b3617f5a0b75d10a0794ad1
zcrow <zcrow@nighwalker-wsl>
2026-02-07 16:10:16 -0500

    Getting output from cuda

--- module3/assignment.cu
+++ module3/assignment.cu
@@ -37,32 +37,32 @@ void init_vectors_basic(int* v1, int* v2)
 // 	}
 // }
 __global__
-void vector_calc(
-	unsigned int* block,
-	int* v1, int* v2, int* v3, int N)
+void vector_calc_stride(int* v1, int* v2, int* v3, int N)
 {
-	const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-	for (int i = 0; i < N; ++i)
+	const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	const unsigned int stride = blockDim.x * gridDim.x;
+	for (int i = idx; i < N; i+=stride)
 	{
-		v3[i] = v1[i] + v2[i];
+		v3[idx] = v1[idx] + v2[idx];
 	}
+}
+__global__
+void vector_calc(int* v1, int* v2, int* v3, int N)
+{
+	const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	if (idx < N)
+		v3[idx] = v1[idx] + v2[idx];
 }
 
 int main(int argc, char** argv)
 {
 	// read command line arguments
-	// int totalThreads = (1 << 20);
-	// int blockSize = 256;
-	// int N = 1000;
-	// int min = -1000;
-	// int max = 1000;
-
-	int totalThreads = 32;
-	int blockSize = 1;
-	int N = 32;
+	int totalThreads = (1 << 20);
+	int blockSize = 256;
+	N = 10000;
 	int min = -1000;
 	int max = 1000;
-	
+
 	if (argc >= 2) {
 		totalThreads = atoi(argv[1]);
 	}
@@ -73,7 +73,7 @@ int main(int argc, char** argv)
 		N = atoi(argv[3]);
 	}
 	printf("Total Threads: %d, Block Size: %d\n", totalThreads, blockSize);
-	printf("Computing %d number of elements\n", N);
+	printf("Computing %ld number of elements\n", N);
 
 	int numBlocks = totalThreads/blockSize;
 
@@ -86,36 +86,44 @@ int main(int argc, char** argv)
 		printf("The total number of threads will be rounded up to %d\n", totalThreads);
 	}
 
-	int num_vecs = 3;
 	long array_size = N;
-	long array_size_in_bytes = sizeof(unsigned int) * array_size * num_vecs;
-	unsigned int *gpu_block;
-	unsigned int *gpu_thread;
+	long array_size_in_bytes = sizeof(int) * array_size;
+	int* gpu_v1;
+	int* gpu_v2;
+	int* gpu_v3;
 
 	// Init vectors
 	int* v1 = (int*)calloc(N , sizeof(int));
 	printf("Allocated v1\n");
 	int* v2 = (int*)calloc(N , sizeof(int));
 	printf("Allocated v2\n");
-	// init_vectors(v1, v2, min, max);
-	init_vectors_basic(v1, v2);
+	init_vectors(v1, v2, min, max);
+	// init_vectors_basic(v1, v2);
 	printf("Initialized both vectors\n");
 	int* v3 = (int*)calloc(N , sizeof(int));
-	unsigned int* cpu_block = (unsigned int*)malloc(num_vecs * N * sizeof(int));
 
-	cudaMalloc((void **)&gpu_block, array_size_in_bytes);
-	cudaMemcpy(cpu_block, gpu_block, array_size_in_bytes, cudaMemcpyHostToDevice);
+	cudaMalloc((void **)&gpu_v1, array_size_in_bytes);
+	cudaMalloc((void **)&gpu_v2, array_size_in_bytes);
+	cudaMalloc((void **)&gpu_v3, array_size_in_bytes);
+	cudaMemcpy(gpu_v1, v1, array_size_in_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_v2, v2, array_size_in_bytes, cudaMemcpyHostToDevice);
 
 	// Kernel
-	vector_calc<<<numBlocks, blockSize>>>(gpu_block, v1, v2, v3, N);
+	vector_calc<<<numBlocks, totalThreads>>>(gpu_v1, gpu_v2, gpu_v3, N);
+
+	cudaDeviceSynchronize();
 
 	// Done with GPU arrays
-	cudaMemcpy(cpu_block, gpu_block, array_size_in_bytes, cudaMemcpyDeviceToHost);
-	cudaFree(gpu_block);
+	cudaMemcpy(v3, gpu_v3, array_size_in_bytes, cudaMemcpyDeviceToHost);
+	cudaFree(gpu_v1);
+	cudaFree(gpu_v2);
+	cudaFree(gpu_v3);
 
-	for (unsigned int i = 0; i < array_size; ++i)
+	for (unsigned int i = array_size-10; i < array_size; ++i)
 	{
 		printf("Vec3 idx %d: %d\n", i, v3[i]);
 	}
-
+	free(v1);
+	free(v2);
+	free(v3);
 }
