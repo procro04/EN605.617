6afc218386e2c8607b0fbd41e571688a7f0627b6
zcrow <zcrow@nighwalker-wsl>
2026-02-22 15:35:30 -0500

    Global mem example complete

--- module5/assignment/assignment.cu
+++ module5/assignment/assignment.cu
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "CodeTimer.h"
 
 long N; // Number of elements
 
-void init_vectors(int* v1, int* v2, int min, int max)
+void init_vectors(int* v1, int* v2)
 {
+    int min = -1000;
+    int max = 1000;
     for (int i = 0; i < N; i++) {
         v1[i] = min + rand() % (max - min + 1);
         v2[i] = min + rand() % (max - min + 1);
@@ -55,35 +58,73 @@ void vector_calc_branch_grid_stride_demo(
     }
 }
 
+void global_memory_test(int numBlocks, int blockSize, int N, int pattern)
+{
+    long array_size = N;
+    long array_size_in_bytes = sizeof(int) * array_size;
+    int *gpu_v1, *gpu_v2, *gpu_v3;
+
+    // Init vectors
+    int* v1 = (int*)calloc(array_size , sizeof(int));
+    int* v2 = (int*)calloc(array_size , sizeof(int));
+    int* v3 = (int*)calloc(N , sizeof(int));
+    init_vectors(v1, v2);
+
+    cudaMalloc((void **)&gpu_v1, array_size_in_bytes);
+    cudaMalloc((void **)&gpu_v2, array_size_in_bytes);
+    cudaMalloc((void **)&gpu_v3, array_size_in_bytes);
+    cudaMemcpy(gpu_v1, v1, array_size_in_bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(gpu_v2, v2, array_size_in_bytes, cudaMemcpyHostToDevice);
+
+    // Kernel
+    std::cout << "Adding vectors\n";
+    CodeTimer timer;
+    timer.startTiming();
+
+    // Execute the kernel
+    vector_calc_branch_grid_stride_demo
+        <<<numBlocks, blockSize>>>(gpu_v1, gpu_v2, gpu_v3, N, pattern);
+    cudaDeviceSynchronize();
+
+    timer.stopTiming();
+    timer.timingResults();
+    std::cout << "Vectors added\n";
+
+    // Done with GPU arrays
+    cudaMemcpy(v3, gpu_v3, array_size_in_bytes, cudaMemcpyDeviceToHost);
+    cudaFree(gpu_v1);
+    cudaFree(gpu_v2);
+    cudaFree(gpu_v3);
+
+    for (unsigned int i = array_size-5; i < array_size; ++i) {
+        std::cout << "Vec3 idx " << i << ":" << v3[i] << "\n";
+    }
+    // Done with host arrays
+    free(v1);
+    free(v2);
+    free(v3);
+}
+
 int main(int argc, char** argv)
 {
     // read command line arguments
     int totalThreads = (1 << 20);
     int blockSize = 256;
     N = 10000;
-    int min = -1000;
-    int max = 1000;
     int pattern = 0;
 
-    if (argc >= 2) {
-        totalThreads = atoi(argv[1]);
-    }
-    if (argc >= 3) {
-        blockSize = atoi(argv[2]);
-    }
-    if (argc >=4) {
-        N = atoi(argv[3]);
-    }
-    if (argc >= 5) {
-        pattern = atoi(argv[4]);
-    }
+    if (argc >= 2) totalThreads = atoi(argv[1]);
+    if (argc >= 3) blockSize = atoi(argv[2]);
+    if (argc >= 4) N = atoi(argv[3]);
+    if (argc >= 5) pattern = atoi(argv[4]);
 
     int numBlocks = totalThreads/blockSize;
-    printf("Total Threads: %d, Block Size: %d, Num Blocks: %d\n", totalThreads, blockSize, numBlocks);
-    printf("Computing %ld number of elements\n", N);
+    std::cout << "Total Threads: " << totalThreads << "\n"
+              << "Block Size: " << blockSize << "\n"
+              << "Num Blocks: " << numBlocks << "\n";
+    std::cout << "Computing " << N << " number of elements\n";
 
-    // Seed the random number generator
-    srand(time(NULL));
+    global_memory_test(numBlocks, blockSize, N, pattern);
 
     // validate command line arguments
     if (totalThreads % blockSize != 0) {
@@ -93,57 +134,4 @@ int main(int argc, char** argv)
         printf("Warning: Total thread count is not evenly divisible by the block size\n");
         printf("The total number of threads will be rounded up to %d\n", totalThreads);
     }
-
-    long array_size = N;
-    long array_size_in_bytes = sizeof(int) * array_size;
-    int* gpu_v1;
-    int* gpu_v2;
-    int* gpu_v3;
-
-    // Init vectors
-    int* v1 = (int*)calloc(N , sizeof(int));
-    printf("Allocated v1\n");
-    int* v2 = (int*)calloc(N , sizeof(int));
-    printf("Allocated v2\n");
-    init_vectors(v1, v2, min, max);
-    // init_vectors_basic(v1, v2);
-    printf("Initialized both vectors\n");
-    int* v3 = (int*)calloc(N , sizeof(int));
-
-    cudaMalloc((void **)&gpu_v1, array_size_in_bytes);
-    cudaMalloc((void **)&gpu_v2, array_size_in_bytes);
-    cudaMalloc((void **)&gpu_v3, array_size_in_bytes);
-    cudaMemcpy(gpu_v1, v1, array_size_in_bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(gpu_v2, v2, array_size_in_bytes, cudaMemcpyHostToDevice);
-
-    // Kernel
-    // vector_calc<<<numBlocks, blockSize>>>(gpu_v1, gpu_v2, gpu_v3, N);
-    struct timespec start, end;
-    double time_spent;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    printf("Adding vectors\n");
-
-    // Execute the kernel
-    vector_calc_branch_grid_stride_demo<<<numBlocks, blockSize>>>(gpu_v1, gpu_v2, gpu_v3, N, pattern);
-    cudaDeviceSynchronize();
-
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    time_spent = (end.tv_sec - start.tv_sec) + 
-    (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Function took %f seconds to execute\n", time_spent);
-    printf("Vectors added\n");
-
-    // Done with GPU arrays
-    cudaMemcpy(v3, gpu_v3, array_size_in_bytes, cudaMemcpyDeviceToHost);
-    cudaFree(gpu_v1);
-    cudaFree(gpu_v2);
-    cudaFree(gpu_v3);
-
-    for (unsigned int i = array_size-10; i < array_size; ++i)
-    {
-        printf("Vec3 idx %d: %d\n", i, v3[i]);
-    }
-    free(v1);
-    free(v2);
-    free(v3);
 }
